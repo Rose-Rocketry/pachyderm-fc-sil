5a9b1b922682c03e26630cec52b3085d10af0364
bry <greatmaharg@gmail.com>
2024-06-17 21:15:32 -0600

    Adapted main to new features, got rid of some junk files

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,16 @@
 const std::string fpath = "test_files";
 const std::string sdpath = fpath+"/sdcard";
 const std::string flashpath = fpath+"/flash.bin";
+const std::string radiopath = fpath+"/radio.bin";
+
+std::ofstream f_radio(radiopath);
 
 FileConfig conf(sdpath);
-StreamSink radio_sink(&std::cout);
+StreamSink debug_sink(&std::cout);
+StreamSink radio_sink(&f_radio);
 FlashImage flash_img(flashpath, true);
 FileStorage fs(sdpath);
-pachy::FCCore core(&radio_sink, &flash_img, &conf, &fs);
+pachy::FCCore core(&debug_sink, &radio_sink, &flash_img, &conf, &fs);
 
 int main(int, char**) {
     std::srand(std::time(nullptr));
